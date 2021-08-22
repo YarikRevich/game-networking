ae692ef987c3aeae5ddb06be9cbc74391f600f57
Yaroslav Svitlytskyi <yariksvitlitskiy81@gmail.com>
2021-08-22 21:37:29 +0300

    feature: added main protocol logic

--- protocol/pkg/models/models.go
+++ protocol/pkg/models/models.go
@@ -1,3 +1,49 @@
 package models
 
-type Preprocessor struct {}
+import (
+	"encoding/json"
+)
+
+var (
+	msgFields = []string{
+		"id",
+		"ping",
+		"data",
+	}
+)
+
+type ID struct {
+	ID string `json:"id"`
+}
+
+type Pinger struct {
+	Ping bool `json:"ping"`
+}
+
+type Data struct {
+	Data interface{} `json:"data"`
+}
+
+//It's a struct which will be given through
+//the dialer and then should be processed by
+//server
+type Msg struct {
+	ID
+	Pinger
+	Data
+}
+
+func IsProtocolMsg(msg []byte)bool {
+	var stub map[string]json.RawMessage
+	if err := json.Unmarshal(msg, &stub); err != nil{
+		return false
+	}
+
+	for _, value := range msgFields{
+		if _, ok := stub[value]; !ok{
+			return false
+		}
+	}
+
+	return true
+}
--- server/internal/workers/workers.go
+++ server/internal/workers/workers.go
@@ -3,6 +3,7 @@ package workers
 import (
 	"net"
 
+	"github.com/YarikRevich/game-networking/protocol/pkg/models"
 	"github.com/YarikRevich/game-networking/server/internal/table"
 	"github.com/YarikRevich/game-networking/server/tools/buffer"
 )
@@ -14,29 +15,31 @@ type WorkerManager struct {
 
 	buff *buffer.Buffer
 	conn *net.UDPConn
-	
+
 	err chan error
 }
 
-func (wm *WorkerManager) Run(){
-	for i := 0; i < wm.count; i++{
+func (wm *WorkerManager) Run() {
+	for i := 0; i < wm.count; i++ {
 		go wm.worker()
 	}
 }
 
-func (wm *WorkerManager) worker(){
+func (wm *WorkerManager) worker() {
 	for {
 		buff, ok := wm.buff.Get().([]byte)
-		if !ok{
+		if !ok {
 			continue
 		}
 
 		_, addr, err := wm.conn.ReadFrom(buff)
-		if err != nil{
+		if err != nil {
 			wm.err <- err
 		}
-		
-		wm.tab.Add(addr.String(), buff)
+
+		if models.IsProtocolMsg(buff) {
+			wm.tab.Add(addr.String(), buff)
+		}
 
 		wm.buff.Put(buff[:0])
 	}
@@ -46,12 +49,11 @@ func (wm *WorkerManager) Error() error {
 	return <-wm.err
 }
 
-
-func New(count int) *WorkerManager{
+func New(count int) *WorkerManager {
 	return &WorkerManager{
 		count: count,
-		tab: table.New(),
-		buff: buffer.New(),
-		err: make(chan error, count),
+		tab:   table.New(),
+		buff:  buffer.New(),
+		err:   make(chan error, count),
 	}
 }
